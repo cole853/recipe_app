71e531325b8bc0cf1e41e07d91803be63abfa0a6
[cole853] <[coleclark75@gmail.com]>
2026-01-11 22:53:00 -0800

    fixed rubocop violations and updated brakeman

--- db/migrate/20260112053556_add_recipe_ingredients_indexes.rb
+++ db/migrate/20260112053556_add_recipe_ingredients_indexes.rb
@@ -3,15 +3,15 @@ class AddRecipeIngredientsIndexes < ActiveRecord::Migration[8.0]
     unless index_exists?(:recipe_ingredients, :recipe_id)
       add_index :recipe_ingredients, :recipe_id
     end
-    
+
     unless index_exists?(:recipe_ingredients, :ingredient_id)
       add_index :recipe_ingredients, :ingredient_id
     end
-    
-    unless index_exists?(:recipe_ingredients, [:ingredient_id, :recipe_id])
-      add_index :recipe_ingredients, [:ingredient_id, :recipe_id]
+
+    unless index_exists?(:recipe_ingredients, [ :ingredient_id, :recipe_id ])
+      add_index :recipe_ingredients, [ :ingredient_id, :recipe_id ]
     end
-    
+
     unless index_exists?(:recipes, :created_at)
       add_index :recipes, :created_at
     end
